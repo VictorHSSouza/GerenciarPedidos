26ea7e8bb23bf66a3ffae6bf2de9f49b3e377d17
VICTOR_HENRIQUE_SANTANA_DE_SOUZA <monitor-victorhenrique@DELTAF52.FLORESTA>
2025-08-13 09:53:05 -0300

    Melhorias na interface: status nos pedidos, CSS centralizado, Bootstrap atualizado e layout modernizado

--- obj/Debug/net8.0/Atividade6_Blazor.AssemblyInfo.cs
+++ obj/Debug/net8.0/Atividade6_Blazor.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Atividade6_Blazor")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8a993d921f99a4739f8f3325586e865b616c171d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+532699c443a554a83b8896a58c3aff2f22b88c64")]
 [assembly: System.Reflection.AssemblyProductAttribute("Atividade6_Blazor")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Atividade6_Blazor")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
